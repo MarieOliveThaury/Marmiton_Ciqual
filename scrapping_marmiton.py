406820b25a5ca69552d9648655d7a52331db83c7
nguesse <nathan.guesse@ensae.fr>
2022-12-12 13:44:19 +0000

    Ajout de la conversion au scrapping marmiton

--- scrapping_marmiton.py
+++ scrapping_marmiton.py
@@ -4,6 +4,98 @@ import html.parser
 import re
 import numpy as np
 from tqdm.auto import tqdm
+import pandas as pd
+
+corres = open("Poids.txt", "r")
+poids=corres.readlines()
+dicopoids={}
+for ligne in poids:
+    dicopoids[str(''.join(z for z in ligne if not z.isdigit()).replace(" \n", "").lower())]=int("".join(re.findall('\d',str(ligne))))
+
+corres2 = open("Quantificateurs.txt", "r")
+quantif=corres2.readlines()
+dicoquantif={}
+for ligne in quantif:
+    dicoquantif[str(''.join(z for z in ligne if not z.isdigit()).replace(" \n", "").lower())]=int("".join(re.findall('\d',str(ligne))))
+
+def convert(dico):
+    for x,y in dico.items():
+        
+            
+                for k,v in y['recette'].items():
+                    if type(v)==str:
+                        if v != '':
+                            if '⁄' in v:
+                                if '1⁄4' in v:
+                                    n=0.25
+                                if '1⁄3' in v:
+                                    n=0.33
+                                if '1⁄2' in v:
+                                    n=0.5
+                                for a,b in dicopoids.items():
+                                    if a in k:
+                                        y['recette'].update({k:str(b*n)+"g"})
+                                for a,b in dicoquantif.items():
+                                    if a in v:
+                                        y['recette'].update({k:str(b*n)+"g"})       
+                            
+                
+                            else:
+                                n=float(("".join(re.findall('\d',str(v)))))
+                
+                                l=str(''.join(z for z in v if not z.isdigit()))
+                
+                                if l not in ['g','.g']:    
+                                    for a,b in dicopoids.items():
+                                        if a in k:
+                                            y['recette'].update({k:str(b*n)+"g"})
+                                    for a,b in dicoquantif.items():
+                                        if a in v:
+                                            y['recette'].update({k:str(b*n)+"g"})       
+                                        
+                        elif v=="": #on définit une valeur standardisé pour les éléments manquants. Ces élements sont souvent utilisés en des quantités limitées.
+                            y['recette'].update({k:str(2*int(y['nombre de personnes']))+"g"})
+                
+def keskimank(dico):
+    for x,y in dico.items():
+                for k,v in y['recette'].items():
+                    l=str(''.join(z for z in v if not z.isdigit()))
+                    if l not in ['.g','g']:
+
+                        print(k,v)
+def bouchetrou(dico):
+    for x,y in dico.items():
+                element=[]
+                for k,v in y['recette'].items():
+                    v=str(v)
+                    l=str(''.join(z for z in v if not z.isdigit()))
+                    if l not in ['.g','g']:
+                        element=element+[k]
+                        
+                for a in element:
+                    del y['recette'][a]
+
+def usable(dico):
+    for x,y in dico.items():
+        for k,v in y['recette'].items():
+                n=float(str(v).replace('g',''))
+                y['recette'].update({k:float(n)})
+                
+def parpersonne(dico):
+    for x,y in dico.items():
+        for k,v in y['recette'].items():
+                
+                y['recette'].update({k:v/y['nombre de personnes']})
+    
+
+                
+def conversion(dico):
+    convert(dico)
+    bouchetrou(dico)
+    usable(dico)
+    parpersonne(dico)
+    
+    
 
 def find_all_dishes(search : str, nb_of_recipes : int): 
 #returns a list with all the url of the recipes corresponding to the research
@@ -87,15 +179,31 @@ def find_recipe(dish_url : str):
     return recipe_title, recipe, nb_people
             
 def find_all_recipes(search : str, nb_of_recipes : int) : 
-#returns a dictionnary with all the the recipes corresponding to the research
+#returns a dataframe with all the the recipes corresponding to the research
     
     list_dishes_url = find_all_dishes(search, nb_of_recipes)
     all_recipes = {}
     for dish in tqdm(list_dishes_url) : 
         all_recipes[find_recipe(dish)[0]] = {'lien' : dish,'recette' : find_recipe(dish)[1], 'nombre de personnes' : find_recipe(dish)[2]}
         #we should add other features like the mark, the nb of people, the number of opinions...
+        
+    recipes_names = list(all_recipes.keys())
+    conversion(all_recipes)
+    df_recipes = []
+    for i in range(len(recipes_names)):
+        dict_recipe = all_recipes[recipes_names[i]]
+        ingredients = list(dict_recipe['recette'].keys())
+        qtes = list(dict_recipe['recette'].values())
     
-    return all_recipes
+        dict_iq = {'Ingrédients' : ingredients, 'Quantités' : qtes}
+        df = pd.DataFrame(dict_iq)
+        df['Nom recette'] = recipes_names[i]
+    
+        df_recipes.append(df)
+    
+    full_df = pd.concat(df_recipes, axis=0, ignore_index=True)
+    
+    return full_df
         
                 
     
