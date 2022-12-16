f4a8c96b8936b77b902ee1e7c7864ed253d48a76
Marie-Olive THAURY <marieolive.thaury@gmail.com>
2022-12-16 18:18:15 +0000

    adding mo graphic function

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 from scrapping.scrapping_ciqual import *
+import plotly.express as px
 
 
-def merge(recipe, nutrition) : 
+def looking_for(search : str, N : int) : 
+    recipes = find_all_recipes(search, N)
+    #recipes.to_csv(r'recipes.csv', index = False)
+    nutritions = nutrition(recipes)
+    #nutritions.to_csv(r'nutritions.csv', index = False)
+    result = merge_and_clean(recipes, nutritions)
+    
+    return result
+
+
+def merge_and_clean(recipe, nutrition) : 
     
     result = recipe.merge(nutrition, on='Ingrédient', how='left')
     result = result.drop_duplicates()
-    result = result.pivot(index=['Nom recette', 'Ingrédient', 'Quantité'], columns='Nutriment', values='Teneur moyenne')
+    result = result.pivot(index=['Nom recette', 'Ingrédient', 'Quantité'], 
+                          columns='Nutriment', values='Teneur moyenne')
     columns = result.columns
 
     result = result.reset_index()
     for i in columns : 
         result[i] = result[i].apply(lambda x : clean(x))
-        result[i] = result[i]*result['Quantité']
+        result[i] = result[i]*result['Quantité']/100
+        if i == 'Protéines, N x 6.25 (g/100 g)' : 
+            result[i] = result[i]*6.25
+    
+    result.rename(columns = {'AG saturés (g/100 g)' : 'AG saturés (g)', 
+                'Energie, Règlement UE N° 1169/2011 (kJ/100 g)' : 'Energie, Règlement UE N° 1169/2011 (kJ)', 
+                'Energie, Règlement UE N° 1169/2011 (kcal/100 g)' : 'Energie, Règlement UE N° 1169/2011 (kcal)',
+                'Glucides (g/100 g)' : 'Glucides (g)', 
+                'Lipides (g/100 g)' : 'Lipides (g)', 
+                'Protéines, N x 6.25 (g/100 g)' : 'Protéines (g)', 
+                'Sel chlorure de sodium (g/100 g)' : 'Sel chlorure de sodium (g)', 
+                'Sucres (g/100 g)' : 'Sucres (g)'}, inplace = True)
     
     return result
+
+def compare_recipes(df_recipes, nutritional_quality : str) :
+    
+    fig = px.bar(df_recipes, x="Nom recette", y=nutritional_quality, color="Ingrédient", 
+                 title=nutritional_quality, width=800, height=500)
+    fig.update_layout(showlegend=False)
+    return fig
 
 
